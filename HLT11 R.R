ff9a9020486a39e3ac4cf04e04c0a22a8d1157c2
Jonathan Tindale <jrtindale@gmail.com>
2022-02-15 18:08:59 +0000

    HLT 12 Homework Uploaded

--- HLT11 R.R
+++ HLT11 R.R
@@ -6,4 +6,3 @@ ggplot2::msleep
 #Density plot of sleep time against diet type to see if there are any patterns that form looking at diet alone
 qplot(sleep_total, data=msleep, geom="density", color=vore, linetype=vore, xlab="Total Sleep (per 24 Hours)", ylab="Density", main="Density plot of Sleep Time")
 
-
