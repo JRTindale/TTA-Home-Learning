53bf90737c8e64617ece5f7c144b17b79f184cec
Jonathan Tindale <jrtindale@gmail.com>
2022-02-25 01:09:37 +0000

    HLT 12 updated and HLT 11

--- HLT 12 SQL.sql
+++ HLT 12 SQL.sql
@@ -2,36 +2,82 @@ Create Database ownedClothes;
 
 USE ownedClothes
 
+Create Table material(
+    ID INT NOT NULL PRIMARY KEY,
+    material VARCHAR(200) NOT NULL
+    );
+
+
 Create Table clothing_details(
-garment_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-item_Type VARCHAR(100) NOT NULL,
-primary_Colour VARCHAR(30) NOT NULL,
-secondary_Colour VARCHAR(30) NULL,
-tertiary_Colour VARCHAR(30) NULL,
-material VARCHAR(200) NOT NULL,
-brand VARCHAR(50) NULL,
-isFormal BIT NOT NULL);
+    garment_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    item_Type VARCHAR(100) NOT NULL,
+    primary_Colour VARCHAR(30) NOT NULL,
+    secondary_Colour VARCHAR(30) NULL,
+    tertiary_Colour VARCHAR(30) NULL,
+    material_ID INT NOT NULL,
+    brand VARCHAR(50) NULL,
+    isFormal BIT NOT NULL,
+    CONSTRAINT FK_MaterialID FOREIGN KEY (material_ID) REFERENCES material(ID)
+    );
+
 
 EXPLAIN clothing_details;
+EXPLAIN material;
 
-INSERT INTO clothing_details(
-item_Type,primary_Colour,secondary_Colour,tertiary_Colour,material,brand,isFormal
+INSERT INTO material(
+ID,material
 )
 VALUES
-('tshirt','black',null,null,'cotton','allSaints',0),
-('tshirt','blue',null,null,'cotton','allSaints',0),
-('tshirt','black',null,null,'cotton','allSaints',0),
-('tshirt','red',null,null,'cotton','allSaints',0),
-('shirt','white',null,null,'cotton','allSaints',0),
-('shirt','green',null,null,'cotton','allSaints',0),
-('shirt','white',null,null,'cotton','TMLewis',1),
-('jeans','black',null,null,'denim','allSaints',0),
-('jeans','indigo',null,null,'denim','armani',0),
-('jeans','lightBlue',null,null,'denim','levis',0),
-('tshirt','green','white',null,'cotton','levis',0),
-('sportsJacket','brown',null,null,'tweed','hugoBoss',1),
-('trainers','cream','brown','blue','quilted','nike',0),
-('shoes','black','yellow',null,'veganLeather','docMarten',0);
+(1,'cotton'),
+(2, 'denim'),
+(3, 'tweed'),
+(4, 'quilted'),
+(5, 'veganLeather');
+
+INSERT INTO clothing_details(
+item_Type,primary_Colour,secondary_Colour,tertiary_Colour,material_id,brand,isFormal
+)
+VALUES
+('tshirt','black',null,null,1,'allSaints',0),
+('tshirt','blue',null,null,1,'allSaints',0),
+('tshirt','black',null,null,1,'allSaints',0),
+('tshirt','red',null,null,1,'allSaints',0),
+('shirt','white',null,null,1,'allSaints',0),
+('shirt','green',null,null,1,'allSaints',0),
+('shirt','white',null,null,1,'TMLewis',1),
+('jeans','black',null,null,2,'allSaints',0),
+('jeans','indigo',null,null,2,'armani',0),
+('jeans','lightBlue',null,null,2,'levis',0),
+('tshirt','green','white',null,1,'levis',0),
+('sportsJacket','brown',null,null,3,'hugoBoss',1),
+('trainers','cream','brown','blue',4,'nike',0),
+('shoes','black','yellow',null,5,'docMarten',0),
+('shoes','black','yellow',null,5,'docMarten',0);
+
+update clothing_details
+set item_Type = 'hoodie'
+where garment_ID = 2;
+
+delete from clothing_details
+where garment_ID = 15;
 
 select *
-from clothing_details;
+from clothing_details cd
+join material m on m.id = cd.material_ID;
+
+select primary_Colour
+from clothing_details cd;
+
+select cd.item_type, m.material, count(1) as numberOfItems
+from clothing_details cd
+join material m on m.id = cd.material_ID
+group by cd.item_type, m.material;
+
+select *
+from clothing_details cd
+join material m on m.id = cd.material_ID
+order by cd.garment_ID;
+
+select *
+from clothing_details cd
+where cd.brand = 'nike';
--- HLT11 R.R
+++ HLT11 R.R
@@ -6,3 +6,75 @@ ggplot2::msleep
 #Density plot of sleep time against diet type to see if there are any patterns that form looking at diet alone
 qplot(sleep_total, data=msleep, geom="density", color=vore, linetype=vore, xlab="Total Sleep (per 24 Hours)", ylab="Density", main="Density plot of Sleep Time")
 
+#1. Write an R program to create three vectors a, b, c with 5 integers. 
+#   Combine the three vectors to become a 3×5 matrix where each column represents a vector. 
+#   Print the content of the matrix. 
+#   Plot a graph and label correctly.
+a <- as.integer(c(1,2,3,4,5))
+b <- as.integer(c(2,4,6,8,10))
+c <- as.integer(c(3,6,9,12,15))
+d <- data.frame(a,b,c)            
+print(d)                          
+
+#install.packages("scatterplot3d")  # Install
+library("scatterplot3d")            # load
+scatterplot3d(x=d$a, xlab = "a vector",
+              y=d$b, ylab = "b vector",
+              z=d$c, zlab = "c vector",
+              main="Plotting 3 vectors",
+              pch = 16, color="Blue")
+
+
+#2. Write a R program to create a Data frames which contain details of 5 employees and display the details. 
+#   (Name, Age, Role and Length of service).
+df2 <- data.frame(
+  Name = c("Kyra","Boris","Alex","Alana","Gary"),
+  Age = c(26,54,32,38,58),
+  Role = c("HR","DataEngineer","BA","SoftwareEngineer","DevOpsEngineer"),
+  LoS = c(1,4,7,4,2)
+)
+df2
+
+
+#3. Import the GGPLOT 2 library and plot a graph using the qplot function. 
+#   X axis is the sequence of 1:20 and the y axis is the x^2. 
+#   Label the graph appropriately. install.packages("ggplot2",dependencies = TRUE)
+library(ggplot2)
+
+x = seq(from = 1, to = 20) 
+y = x^2                   
+df <- data.frame(x,y)    
+
+ggplot(data=df, aes(x=x, y=y)) +
+  geom_line()+
+  geom_point()+
+  labs(
+    title="Plotting x squared for the values 1 to 20",
+    x="Numbers 1 to 20",
+    y="X^2")
+
+#4. Create a simple bar plot of five subjects
+Subject = c("Maths","Chemistry","Physics","Biology","Art")
+AttendanceCount <- sample(1:100, 5, replace=F)
+barplot(
+  AttendanceCount,
+  names.arg=Subject,
+  xlab="Subject",
+  ylab="Count of Attendance",
+  main="Count of students attending per subject")
+
+#5. Write a R program to take input from the user (name and age) and display the values.
+my.name <- readline(prompt="Enter name:")
+my.age <- as.integer(readline(prompt="Enter age:"))
+print(paste("Hi, ", my.name, " you are ", my.age, " years old."))
+
+#6. Write a R program to create a sequence of numbers from 20 to 50 and find the mean of numbers from 20 to 50 and sum of numbers.
+x <- seq(from = 20, to = 50)
+MeanX = mean(x)
+SumX = sum(x)
+MeanX
+SumX
+
+#7. Write a R program to create a vector which contains 10 random integer values between -50 and +50
+x <- sample(-50:50, 10)
+x
